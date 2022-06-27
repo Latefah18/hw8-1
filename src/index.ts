1295ded1c4046952169db254ab97796381794c61
Latefah18 <106754794+Latefah18@users.noreply.github.com>
2022-06-27 07:54:03 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { server } from './server';
 
-const port: any = process.env.$PORT ?? 3000
+const port: any = process.env.PORT ?? process.env.$PORT ?? 3002;
 
 server
 	.listen({
@@ -11,3 +11,4 @@ server
 		server.log.error(err);
 		process.exit(1);
 	});
+
